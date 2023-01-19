6f3ef6948d5069127176ecfb053377a5c2470c00
Hasibul Hamid <hamidhasibul@gmail.com>
2023-01-19 23:14:05 +0600

    Server test in office 19/1/2023

--- server.js
+++ server.js
@@ -1,26 +1,62 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import seedRouter from "./routes/seedRoutes.js";
-import CategoryRouter from "./routes/categoryRoutes.js";
-import UserRouter from "./routes/userRoutes.js";
-import ProductRouter from "./routes/productRoutes.js";
-import OrderRouter from "./routes/orderRoutes.js";
-import PostRouter from "./routes/postRoutes.js";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import CategoryRouter from './routes/categoryRoutes.js';
+import UserRouter from './routes/userRoutes.js';
+import ProductRouter from './routes/productRoutes.js';
+import OrderRouter from './routes/orderRoutes.js';
+import PostRouter from './routes/postRoutes.js';
+import twilio from 'twilio';
+import axios from 'axios';
 
 const app = express();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(
+  'AC307ade278258d846a781744619ecade0',
+  '040f4722ba4709bd9a2629022632e70e'
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Router
 
-app.use("/api/seed/", seedRouter);
-app.use("/api/category/", CategoryRouter);
-app.use("/api/products/", ProductRouter);
-app.use("/api/users/", UserRouter);
-app.use("/api/orders/", OrderRouter);
-app.use("/api/posts/", PostRouter);
+app.use('/api/seed/', seedRouter);
+app.use('/api/category/', CategoryRouter);
+app.use('/api/products/', ProductRouter);
+app.use('/api/users/', UserRouter);
+app.use('/api/orders/', OrderRouter);
+app.use('/api/posts/', PostRouter);
+
+// Sending SMS
+
+app.use(`/api/sendSms`, (req, res) => {
+  /* client.messages
+    .create({
+      to: '+8801826705615',
+      from: '+19259405518',
+      body: 'hello from ctg e market',
+    })
+    .then((message) => console.log(message.status)); */
+  sendOtp();
+
+  res.send('Sent SmS');
+});
+
+function sendOtp() {
+  var num = '8801700951458';
+  var msg = 'Hello From Test';
+  axios
+    .post('https://sowdaapp.com/sendotp.php', {
+      num: num,
+      msg: msg,
+    })
+    .then((res) => console.log(''))
+    .catch((err) => console.log(err));
+}
 
 // Connect with DB
 
@@ -29,7 +65,7 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log("connected to DB");
+    console.log('connected to DB');
   })
   .catch((err) => {
     console.log(err.message);
