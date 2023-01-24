771d906dfa847b8164fb59da89cfaa3548a8bdec
hasib <hamidhasibul@gmail.com>
2023-01-25 02:04:10 +0600

    Slight Modifications 25/1/2023

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,10 +1,17 @@
-import express from 'express';
-import Order from '../models/orderModel.js';
+import express from "express";
+import Order from "../models/orderModel.js";
 
 const OrderRouter = express.Router();
 
+// Get all Orders
+
+OrderRouter.get("/", async (req, res) => {
+  const order = await Order.find();
+  res.send(order);
+});
+
 //Create Order
-OrderRouter.post('/', async (req, res) => {
+OrderRouter.post("/", async (req, res) => {
   const newOrder = new Order({
     orderItems: req.body.orderItems.map((x) => ({ ...x, product: x._id })),
     id: req.body.id,
@@ -18,28 +25,28 @@ OrderRouter.post('/', async (req, res) => {
     totalPrice: req.body.totalPrice,
   });
   const order = await newOrder.save();
-  res.status(201).send({ message: 'New Order Created', order });
+  res.status(201).send({ message: "New Order Created", order });
 });
 
 //Get my orders
-OrderRouter.get('/mine/:id', async (req, res) => {
+OrderRouter.get("/mine/:id", async (req, res) => {
   const orders = await Order.find({ id: req.params.id });
   res.send(orders);
 });
 
 //Get my One Order
-OrderRouter.get('/:id', async (req, res) => {
+OrderRouter.get("/:id", async (req, res) => {
   const order = await Order.findById(req.params.id);
   if (order) {
     res.send(order);
   } else {
-    res.status(404).send({ message: 'Order Not Found' });
+    res.status(404).send({ message: "Order Not Found" });
   }
 });
 
 //Get my orders
 //Get my orders
-OrderRouter.get('/userorders/:id', async (req, res) => {
+OrderRouter.get("/userorders/:id", async (req, res) => {
   const orders = await Order.find({ sellerId: req.params.id });
   res.send(orders);
 });
